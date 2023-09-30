a77107510475e2a41c8570726bdc1d4d934fc3bf
anik <anik11556@gmail.com>
2023-09-30 23:52:10 +0600

    done

--- lib/SM/Firebase/FirebaseMethods/FirebaseFetchdata.dart
+++ lib/SM/Firebase/FirebaseMethods/FirebaseFetchdata.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+import '../../ModelClass/Post.dart';
 import '../../ModelClass/User.dart';
 
 class FirebaseFetchData{
@@ -9,11 +10,16 @@ class FirebaseFetchData{
   final String collectionName;
   FirebaseFetchData(this.collectionName);
 
-  Future<List<User>> fetchdata() async {
+  Future<List<Object>> fetchdata() async {
     if(collectionName == "users")
     {
       List<User> rt= await fetchUser();
-      return rt;
+      return rt as List<User>;
+    }
+    else if(collectionName == "posts")
+    {
+      List<Post> rt= await fetchPost();
+      return rt as List<Post>;
     }
     else {
       return [];
@@ -74,5 +80,67 @@ class FirebaseFetchData{
 
   }
 
+  Future<List<Post>> fetchPost() async {
+
+    print("Post Fetching");
+    CollectionReference collection = FirebaseFirestore.instance.collection('all_Posts');
+    QuerySnapshot querySnapshot = await collection.get();
+
+
+    querySnapshot.docs.forEach((doc) {
+
+ /*     String bio = doc.get('bio');
+      String country = doc.get('country');
+      String email = doc.get('email');
+      String gender = doc.get('gender');
+      String id = doc.get('id');
+      bool isOnline = doc.get('isOnline');
+      Timestamp lastSeen = doc.get('lastSeen');
+      String photoUrl = doc.get('photoUrl');
+      String userName = doc.get('username');
+      Timestamp time =  doc.get('time');
+*/
+
+      String? id = doc.get('id');
+      String? postId = doc.get('postId');
+      String? userName = doc.get('userName');
+      String? ownerId = doc.get('ownerId');
+      String? location = doc.get('location');
+      String? timestamp = doc.get('timestamp');
+      String? mediaUrl = doc.get('mediaUrl');
+      String? description = doc.get('description');
+      Post curPost =  Post.Complete(id,postId,userName,ownerId,location,timestamp,mediaUrl,description);
+      bool exists = Post.posts.any((entity) => entity.postId == curPost.postId);
+
+      // print(curUser.userName);
+      // //print(DateFormat("MMMM d, y 'at' h:mm:ss a 'UTC'z").format(curUser.lastSeen.toDate()).toString());
+      // print(curUser.time.toDate());
+      if(exists)
+      {
+        print("already in the list");
+      }
+      else
+      {
+        print("not exist in the list");
+        Post.posts.add(curPost);
+      }
+
+    });
+
+
+    print(Post.posts.length);
+/*
+
+
+    JSONMethods wj = JSONMethods();
+    wj.writeToJSON(User.users);
+
+*/
+
+    print("writeDOne");
+
+    return Post.posts;
+
+  }
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:dhabiansomachar/SM/Firebase/FirebaseMethods/FirebaseFetchdata.dart';
+import 'package:dhabiansomachar/SM/ModelClass/Post.dart';
 import 'package:dhabiansomachar/SM/ModelClass/User.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -37,97 +38,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
- /* Future<void> fetchUser() async {
-
-    print("hello world");
-    CollectionReference collection = FirebaseFirestore.instance.collection('users');
-    QuerySnapshot querySnapshot = await collection.get();
-
-
-    querySnapshot.docs.forEach((doc) {
-
-     // print(doc.get('id'));
-
-
-      String bio = doc.get('bio');
-      String country = doc.get('country');
-      String email = doc.get('email');
-      String gender = doc.get('gender');
-      String id = doc.get('id');
-      bool isOnline = doc.get('isOnline');
-      Timestamp lastSeen = doc.get('lastSeen');
-      String photoUrl = doc.get('photoUrl');
-      String userName = doc.get('username');
-      Timestamp time =  doc.get('time');
-
-
-      User curUser = new  User.Complete(userName,email,country,bio,gender,photoUrl,id,lastSeen,isOnline,time);
-      bool exists = User.users.any((entity) => entity.email == curUser.email);
-
-      // print(curUser.userName);
-      // //print(DateFormat("MMMM d, y 'at' h:mm:ss a 'UTC'z").format(curUser.lastSeen.toDate()).toString());
-      // print(curUser.time.toDate());
-      if(exists)
-      {
-        print("already in the list");
-      }
-      else
-      {
-        print("not exist in the list");
-        User.users.add(curUser);
-      }
-
-      // String mediaUrl = doc.get('mediaUrl');
-
-
-*//*
-      UserModel data =  UserModel("anik", "anik11556@gmail.com", "userID", "https://devdiscourse.blob.core.windows.net/devnews/17_07_2019_19_18_59_861541.jpg", "e", "f", "h");
-      data.username =userName;
-      data.email = email;
-      data.id = id;
-      data.photoUrl =photourl;
-      data.bio =bio;
-      data.country = country;
-
-
-      print(data.email);
-
-      bool exists = UserModel.um.any((entity) => entity.email == data.email);
-      if(exists)
-      {
-        print("already in the list");
-      }
-      else
-      {
-        print("not exist in the list");
-        UserModel.um.add(data);
-      }
-
-
-
-    });
-
-    print(UserModel.um.length);
-    setState(() {
-      isLoading = false;
-
- *//*
-    });
-
-
-    print(User.users.length);
-
-
-    JSONMethods wj = JSONMethods();
-    wj.writeToJSON(User.users);
-
-
-    print("writeDOne");
-
-
-  }
-*/
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -164,10 +74,20 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           FirebaseFetchData ffd = FirebaseFetchData("users");
-          List<User> us = await ffd.fetchdata() ;
-
+          List<Object> uk = await ffd.fetchdata() ;
+          List<User> us = uk as List<User>;
           print("Runing from main");
           us.forEach((element) {print(element.userName);});
+
+          print("printing fetch Post");
+
+          FirebaseFetchData fd = FirebaseFetchData("posts");
+          List<Object> ukw = await fd.fetchdata() ;
+          List<Post> usw = ukw as List<Post>;
+          print("Runing from main");
+          usw.forEach((element) {print(element.description);});
+
+
         },//fetchUser,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
